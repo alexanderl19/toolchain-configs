f99a9887a113553246810fc0756aeec4d410ed4f
Alexander Liu <a@alexanderliu.com>
2025-07-01 00:58:23 -0700

    feat: ts svelte flat eslint config

---
+++ packages/alexanderliu-eslint-config-typescript-svelte/index.js
@@ -0,0 +1,82 @@
+import prettier from "eslint-config-prettier";
+import { includeIgnoreFile } from "@eslint/compat";
+import js from "@eslint/js";
+import svelte from "eslint-plugin-svelte";
+import globals from "globals";
+import { fileURLToPath } from "node:url";
+import ts from "typescript-eslint";
+import svelteConfig from "./svelte.config.js";
+
+const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url));
+
+const alexanderliuEslintConfig = ts.config(
+	includeIgnoreFile(gitignorePath),
+	js.configs.recommended,
+	...ts.configs.recommended,
+	{
+		rules: {
+			"import/export": "error",
+			"import/no-named-as-default": "warn",
+			"import/no-named-as-default-member": "warn",
+			"import/default": "error",
+			"import/namespace": "error",
+			"import/no-duplicates": "error",
+			"import/consistent-type-specifier-style": ["error", "prefer-top-level"],
+			"import/order": [
+				"error",
+				{
+					groups: [
+						"type",
+						"builtin",
+						"external",
+						"internal",
+						"parent",
+						"sibling",
+						"index",
+					],
+					"newlines-between": "always",
+					pathGroups: [
+						{
+							pattern: "$app/**",
+							group: "external",
+							position: "before",
+						},
+						{
+							pattern: "$env/**",
+							group: "external",
+							position: "before",
+						},
+						{
+							pattern: "$lib/**",
+							group: "internal",
+						},
+					],
+					distinctGroup: true,
+					pathGroupsExcludedImportTypes: ["type"],
+				},
+			],
+		},
+	},
+	...svelte.configs.recommended,
+	prettier,
+	...svelte.configs.prettier,
+	{
+		languageOptions: {
+			globals: { ...globals.browser, ...globals.node },
+		},
+		rules: { "no-undef": "off" },
+	},
+	{
+		files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				extraFileExtensions: [".svelte"],
+				parser: ts.parser,
+				svelteConfig,
+			},
+		},
+	},
+);
+
+export { alexanderliuEslintConfig };
